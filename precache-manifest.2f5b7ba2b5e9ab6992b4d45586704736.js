af3b6e288efc29fc9babfb1a20c9ed62d0ee653b
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 10:27:55 +0530

    Updates

---
+++ precache-manifest.2f5b7ba2b5e9ab6992b4d45586704736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb601d378a5c39fbd6575638d0d1da37",
+    "url": "/calculate/index.html"
+  },
+  {
+    "revision": "422a14cd93be42bb1e11",
+    "url": "/calculate/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/calculate/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "422a14cd93be42bb1e11",
+    "url": "/calculate/static/js/main.37ccaeae.chunk.js"
+  },
+  {
+    "revision": "d7a47bb66fbc5b0e3ac2",
+    "url": "/calculate/static/js/runtime~main.b326a65f.js"
+  }
+]);
